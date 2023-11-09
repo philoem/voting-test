fb76f9d21cea33131d879e1b97820b31213fb135
Philoem <philoem24@gmail.com>
2023-11-09 20:32:33 +0100

    build: add hardhat storage layout

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('hardhat-storage-layout');
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
