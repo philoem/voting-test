6fb18fc6d49936fb97d2ff0761cd0f852cdd15f3
Philoem <philoem24@gmail.com>
2023-11-12 15:06:05 +0100

    build: add package hardhat gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@primitivefi/hardhat-dodoc';
 import 'dotenv/config';
+import '@typechain/hardhat';
+import "hardhat-gas-reporter";
 require('hardhat-storage-layout');
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
@@ -9,7 +11,15 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    }
+  },
   defaultNetwork: "hardhat",
   networks: {
     goerli: {
@@ -33,6 +43,9 @@ const config: HardhatUserConfig = {
     debugMode: true,
     // More options...
   },
+  mocha: {
+    timeout: 500000,
+  }
 };
 
 export default config;
