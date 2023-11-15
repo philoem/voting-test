7cac58a6d188f07d3f46c38d9905846e50bbc11a
Philoem <philoem24@gmail.com>
2023-11-15 13:59:20 +0100

    build: add private key from coinmarketcap to check gas consumer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,17 +9,27 @@ require('hardhat-storage-layout');
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    }
-  },
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          outputSelection: {
+            "*": {
+              "*": ["storageLayout"],
+            },
+          },
+        },
+      }
+    ]
+  },  
   defaultNetwork: "hardhat",
   networks: {
     goerli: {
@@ -35,8 +45,8 @@ const config: HardhatUserConfig = {
     enabled: true,
     outputFile: "gas-report.txt",
     noColors: true,
-    currency: "USD",
-    // coinmarketcap: COINMARKETCAP_API_KEY,
+    currency: "EUR",
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   dodoc: {
     runOnCompile: true,
