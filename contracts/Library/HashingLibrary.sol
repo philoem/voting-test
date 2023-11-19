160f89036cdbae4f6db4deb84c114fc234282974
Philoem <philoem24@gmail.com>
2023-11-19 15:21:30 +0100

    feat: add hashing library

---
+++ contracts/Library/HashingLibrary.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+  function hashingVote(bytes32 _candidate) pure returns (uint256) {
+    bytes memory candidat = abi.encodePacked(_candidate);
+    uint hash = uint(keccak256(candidat));
+    return hash;
+  }
+
+  library Hashing {
+    function stringToUint(string calldata _str) public pure returns (uint256) {
+      bytes memory strBytes = bytes(_str);
+      bytes32 hash = keccak256(strBytes);
+      uint256 result = uint256(hashingVote(hash));
+      return result;
+    }
+  }
