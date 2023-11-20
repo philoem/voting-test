1af52561c4047ee9506923a0b3ce6c7e92cd8662
Philoem <philoem24@gmail.com>
2023-11-20 11:45:20 +0100

    feat: create page of front

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [candidat, setCandidat] = useState('')
   const [isVoted, setIsVoted] = useState(false)
+  const [voter, setVoter] = useState('')
+  const [isConnected, setIsConnected] = useState(false)
+  const [nbrOfStarWArs, setNbrOfStarWArs] = useState(0)
+  const [nbrStarTrek, setNbrStarTrek] = useState(0)
+
+  useEffect(() => {
+    isConnected === false ? console.log('Votant non connecté à son wallet') : console.log('Votant connecté à son wallet')
+    isVoted === true ? console.log('voter is already voted') : console.log('voter is not yet voted')
+  })
+  
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window
+      if (!ethereum) {
+        alert('Get MetaMask!')
+        return
+      }
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+      console.log('Connected', accounts[0])
+      setVoter(accounts[0])
+      setIsConnected(true)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const starWarsVote = async () => {
+    setNbrOfStarWArs(nbrOfStarWArs + 1)
+    setIsVoted(true)
+  }
+
+  const starTrekVote = async () => {
+    setNbrStarTrek(nbrStarTrek + 1)
+    setIsVoted(true)
+  }
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>Votez pour votre candidat préféré</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Simply Vote</h1>
+      {!isConnected && (        
+        <div className="card">
+          <button onClick={() => connectWallet()}>
+            Connect your wallet
+          </button>
+          <p>
+            Enjoy !
+          </p>
+        </div>
+      )}
+      {isConnected && (
+        <>
+          <p className="read-the-docs">
+            Which prefer between :
+          </p>
+          <button onClick={() => starWarsVote()} disabled={isVoted}>
+            Star Wars
+          </button>
+          <button onClick={() => starTrekVote()} disabled={isVoted}>
+            Star Trek
+          </button>
+          {isVoted && (
+            <>
+              <p>
+                Thank you for voting !
+              </p>
+              <p>
+                {voter}
+              </p>
+              <p>
+                Star Wars : {nbrOfStarWArs}
+              </p>
+              <p>
+                Star Trek : {nbrStarTrek}
+              </p>
+            </>
+          )}
+        </>
+      )} 
     </>
   )
 }
