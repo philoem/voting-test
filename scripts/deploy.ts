febd3e336ebcf86fa63650aea51f8cf7e72089f6
Philoem <philoem24@gmail.com>
2023-11-20 12:18:47 +0100

    build: add deploy script and run script into goerli's testnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,29 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Voting = await ethers.getContractFactory("Voting", {  libraries: { Hashing: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' } });
+  const voting = await Voting.deploy('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
 
-  const lockedAmount = ethers.parseEther("0.001");
+  await voting.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  console.log(`Voting deployed to ${voting.target}`);
 
-  await lock.waitForDeployment();
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const unlockTime = currentTimestampInSeconds + 60;
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // const lockedAmount = ethers.parseEther("0.001");
+
+  // const lock = await ethers.deployContract("Lock", [unlockTime], {
+  //   value: lockedAmount,
+  // });
+
+  // await lock.waitForDeployment();
+
+  // console.log(
+  //   `Lock with ${ethers.formatEther(
+  //     lockedAmount
+  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  // );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
