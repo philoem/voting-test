75562b47f8b791e19c3cf9e1f76be725326e39ce
Philoem <philoem24@gmail.com>
2023-11-21 16:36:13 +0100

    test: update eligibility's tests

--- test/Eligibility.ts
+++ test/Eligibility.ts
@@ -7,19 +7,22 @@ describe("Eligibility", () => {
   let myContract: Eligibility & { deploymentTransaction(): ContractTransactionResponse; };
   let admin: any;
   let eligibleVoter1: any;
+  let eligibleVoter2: any;
 
   beforeEach(async () => {
-    [admin, eligibleVoter1] = await ethers.getSigners();
+    [admin, eligibleVoter1, eligibleVoter2] = await ethers.getSigners();
 
     const MyContract = await ethers.getContractFactory('Eligibility');
     myContract = await MyContract.deploy();
     await myContract.getDeployedCode();
   });
 
-  it("should not allow non-admin to add eligible voters", async () => {
-    await expect(myContract.connect(eligibleVoter1).addEligibleVoter(eligibleVoter1.address)).to.be.revertedWith(
-      "Only admin can add eligible voters"
-    );
+  it("should return list of eligible voters", async () => {
+    const voters = await myContract.getVoters();
+    expect(voters.length).to.equal(3); 
+    expect(voters[0]).to.equal(admin.address);
+    expect(voters[1]).to.equal(eligibleVoter1.address);
+    expect(voters[2]).to.equal(eligibleVoter2.address);
   });
   
 });
