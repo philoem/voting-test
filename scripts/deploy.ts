f4133886530e38c30e3aedfbe58615169f682a6c
Philoem <philoem24@gmail.com>
2023-12-05 13:44:03 +0100

    refactor: remove comment unused

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -18,23 +18,6 @@ async function main() {
     const targetString = String(voting.target);
     await verify(targetString, ['0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266']);
   }
-
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  // const unlockTime = currentTimestampInSeconds + 60;
-
-  // const lockedAmount = ethers.parseEther("0.001");
-
-  // const lock = await ethers.deployContract("Lock", [unlockTime], {
-  //   value: lockedAmount,
-  // });
-
-  // await lock.waitForDeployment();
-
-  // console.log(
-  //   `Lock with ${ethers.formatEther(
-  //     lockedAmount
-  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  // );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
