b1167d96c69689019398f0249f8265cd093e99ea
Philoem <philoem24@gmail.com>
2023-12-07 19:06:56 +0100

    feat: add new methods to commit & reveal vote with display winner's method

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,15 +1,14 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Hashing } from './Library/HashingLibrary.sol';
+import './Ownable.sol';
 import './MerkleTreeProof.sol';
+import { Hashing } from './Library/HashingLibrary.sol';
 
-contract Voting {
-  
-  MerkleTreeProof merkleTreeProofInstance;
+contract Voting is Ownable {
 
   struct Voter {
-    bool voted;
+    bool voted; 
     uint vote;
     bool committed;
     bytes32 encryptedVote;
@@ -18,35 +17,52 @@ contract Voting {
   mapping(address => Voter) public voters;
   mapping(uint256 => uint256) public voteCount;
   mapping(address => bytes32) public commitHashes;
+
+  string[] public winnerIs;
+
+  bytes32 private merkleTreeRoot;
+
+  event WinnerIs(string candidate);
   
-  constructor(address _merkleTreeProofInstance) {
-    merkleTreeProofInstance = MerkleTreeProof(_merkleTreeProofInstance);
+  constructor(bytes32 _merkleTreeProofInstance, bytes32[] memory _proof, bytes32 _leaf) {
+    MerkleTreeProof merkleTreeProofInstance = new MerkleTreeProof(_merkleTreeProofInstance);
+    if(!merkleTreeProofInstance.verify(_proof, _leaf)) {
+      revert("You're not on the list, you can't vote");
+    }
   }
 
-  function commitVote(bytes32 _commitHash) internal {
-    require(!voters[msg.sender].committed, "Vote has already been committed");
+  function commitVote(address voterAuthorized, string calldata _candidate) internal onlyvoterAuthorized returns (bytes32) {
+    require(!voters[voterAuthorized].committed, "Vote has already been committed");
 
-    commitHashes[msg.sender] = _commitHash;
-    voters[msg.sender].committed = true;
+    bytes32 commitHash = keccak256(abi.encodePacked(_candidate, Hashing.generatedSaltToCommitHash()));
+    require(commitHashes[voterAuthorized] == commitHash, "Invalid commit hash");
+    voters[voterAuthorized].committed = true;
+
+    return commitHash;
   }
 
-  function vote(string calldata _candidate) public returns(uint256) {
-    bytes32 addressBytes = keccak256(abi.encodePacked(msg.sender));
-    require(merkleTreeProofInstance.merkleRoot() == addressBytes, "Invalid merkle root");
-    require(!voters[msg.sender].voted, "Voter has already voted");
+  function vote(string calldata _candidate) public onlyvoterAuthorized returns(uint256) {    
+    require(!voters[voterAuthorized].voted, "Voter has already voted");
 
     // Commit-reveal system
-    require(voters[msg.sender].committed);
-    bytes32 commitHash = keccak256(abi.encodePacked(_candidate, Hashing.generatedSaltToCommitHash()));
-    commitVote(commitHash);
-    require(commitHashes[msg.sender] == commitHash, "Invalid commit hash");
+    commitVote(voterAuthorized, _candidate);
+    require(voters[voterAuthorized].committed);
 
     uint uintegerString = Hashing.stringToUint(_candidate);
     
-    voters[msg.sender].voted = true;
-    voters[msg.sender].vote = uintegerString;
+    voters[voterAuthorized].voted = true;
+    voters[voterAuthorized].vote = uintegerString;
     
     return voteCount[uintegerString] += 1;
+  }
+
+  function revealVote(string calldata _answer) public {
+    require(commitVote(msg.sender, _answer) == commitHashes[msg.sender], "You are an imposter");
+    winnerIs.push(_answer);
+  }
+
+  function isWinner() public {
+    emit WinnerIs(winnerIs[0]);
   }
 
 }
